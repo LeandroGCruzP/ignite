9e990d6bafb4825e759ebf21bc77bb87a52ab4d7
Leandro Cruz <leandro.programmer@hotmail.com>
2021-07-26 19:05:00 -0300

    aula 2

---
+++ github-explorer/dist/bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Home = Home;
+
+function Home() {
+  return /*#__PURE__*/React.createElement("h1", null, "Hello World");
+}
