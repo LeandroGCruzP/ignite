94a7f5ab909504ba3dd768d8e81b46459450fc5c
leh <leandro.programmer@hotmail.com>
2021-08-26 23:45:26 -0300

    formatando Modal

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
 import Modal from 'react-modal'
+import { useState } from 'react'
 
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
 
 import { GlobalStyle } from './styles/global'
+import { NewTransactionModal } from './components/NewTransactionModal'
 
-Modal.setAppElement('#root')  // Acesibilidade
+Modal.setAppElement('#root') // Acesibilidade
 
 export function App() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false)
 
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true)
@@ -24,9 +26,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} >
-          <h2>Cadastrar transacao</h2>
-        </Modal>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
 
       <GlobalStyle />
     </>
